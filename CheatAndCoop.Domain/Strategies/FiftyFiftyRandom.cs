333dc9a081ecd37f6b6169a86cface0332a8b2b0
David Rovani <drovani@blueboltsolutions.com>
2020-11-14 16:48:57 -0600

    Added 50/50 random strategy.

---
+++ CheatAndCoop.Domain/Strategies/FiftyFiftyRandom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CheatAndCoop.Strategies
+{
+    public class FiftyFiftyRandom : IStrategy
+    {
+        protected Random gen { get; init; } = new Random();
+        public Choice GetNextChoice(IEnumerable<Choice> ownChoices, IEnumerable<Choice> opponentChoices)
+        {
+            if (gen.NextDouble() >= 0.5) return Choice.Cheat;
+            else return Choice.Coop;
+        }
+    }
+}
