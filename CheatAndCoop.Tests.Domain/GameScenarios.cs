a8949f1d0a1299575563d59a4af4b7cb42df0570
David Rovani <drovani@blueboltsolutions.com>
2020-11-14 20:36:36 -0600

    Added more helpers to support adding strategy tests.

--- CheatAndCoop.Tests.Domain/GameScenarios.cs
+++ CheatAndCoop.Tests.Domain/GameScenarios.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CheatAndCoop.Tests.Domain
@@ -7,5 +8,15 @@ namespace CheatAndCoop.Tests.Domain
     {
         public static IEnumerable<Choice> AllCoop(int rounds) => Enumerable.Repeat(Choice.Coop, rounds);
         public static IEnumerable<Choice> AllCheat(int rounds) => Enumerable.Repeat(Choice.Cheat, rounds);
+        public static IEnumerable<Choice> RandomChoices(int rounds)
+        {
+            var rng = new Random();
+            var choices = new List<Choice>(rounds);
+            for(int i = 0; i < rounds; i++)
+            {
+                choices.Add(rng.NextDouble() >= 0.5 ? Choice.Cheat : Choice.Coop);
+            }
+            return choices;
+        }
     }
 }
---
+++ CheatAndCoop.Tests.Domain/Strategies/StrategyTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace CheatAndCoop.Tests.Domain.Strategies
+{
+    public abstract class StrategyTests<TStrategy> : StrategyTests where TStrategy : IStrategy, new()
+    {
+        protected StrategyTests(Choice firstChoiceShouldBe)
+        {
+            Strategy = new TStrategy();
+            FirstChoiceShouldBe = firstChoiceShouldBe;
+        }
+    }
+
+    public abstract class StrategyTests
+    {
+        public int RoundsToTest { get; init; } = 10;
+        public IStrategy Strategy { get; init; }
+        public Choice FirstChoiceShouldBe { get; init; }
+
+        [TestMethod]
+        public void FirstChoiceMatches()
+        {
+            Choice choice = Strategy.GetNextChoice(Enumerable.Empty<Choice>(), Enumerable.Empty<Choice>());
+
+            Assert.AreEqual(FirstChoiceShouldBe, choice);
+        }
+    }
+}
